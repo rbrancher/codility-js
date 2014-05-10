59e20fee073a2c609b40128a2c2250af23899ef5
rodrigo brancher <rbrancher@gmail.com>
2014-05-10 06:16:56 -0300

    solve MaxCounters with 100/80

--- 2-counting_elements/MaxCounters.js
+++ 2-counting_elements/MaxCounters.js
@@ -1,17 +1,15 @@
 function solution(N, A) {
     var counters = new Array(N); // 0 is max
 
-    function max_counter() {
-        return counters[0];
-    }
-
     for (var j = 0; j <= N; j++) {
         counters[j] = 0;
     }
 
     for (var i = 0, n = A.length; i < n; i++) {
         if (A[i] > N) { // max_counters
-            counters = counters.map(max_counter);
+            for (var k = 1; k <= N; k++) {
+                counters[k] = counters[0];
+            }
         } else { // increase counter
             counters[A[i]]++;
             if (counters[A[i]] > counters[0]) {
