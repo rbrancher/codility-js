c5814528bb3120a52e6c0245508b84d43c7de565
rodrigo brancher <rbrancher@gmail.com>
2014-06-12 13:15:09 -0300

    solves NumberOfDiscsIntersections with 100/100

--- 4-sorting/NumberOfDiscIntersections.js
+++ 4-sorting/NumberOfDiscIntersections.js
@@ -1,23 +1,30 @@
 function solution(A) {
     var n = A.length,
         i = 0,
-        left_limit = [],
-        right_limit = [],
+        discs = [],
         intersections = 0,
         LIMIT = 10000000;
 
     for (; i < n; i++) {
-        left_limit[i] = i - A[i] <= 0 ? 0 : i - A[i];
-        right_limit[i] = i + A[i] >= n ? n : i + A[i];
+        discs[i] = {
+            left: i - A[i] <= 0 ? 0 : i - A[i],
+            right: i + A[i] >= n ? n : i + A[i]
+        };
     }
 
+    discs.sort(function (a, b) {
+        return a.left - b.left;
+    });
+
     for (i = 0; i < n; i++) {
-        for (var k = i + 1; k <= n ; k++) {
-            if (right_limit[i] >= left_limit[k]) {
+        for (var k = i + 1; k < n ; k++) {
+            if (discs[i].right >= discs[k].left) {
                 intersections++;
                 if (intersections >= LIMIT) {
                     return -1;
                 }
+            } else {
+                break;
             }
         }
     }
@@ -26,3 +33,5 @@ function solution(A) {
 }
 
 // solve with 100/37 - https://codility.com/demo/results/demo568B7X-93W/
+// solve with 100/87 - https://codility.com/demo/results/demo93WJZK-HDE/
+// solver with 100/100 - https://codility.com/demo/results/demoM7TDGV-RG4/
